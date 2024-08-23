7e5ff2b2927e7449a4404fa9f786e3c336c8a4e6
Guilhermefariah <guilhermefaria321@gmail.com>
2024-08-23 20:18:04 -0300

    Mudanças na cores do projestos e melhorando pagina home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,12 @@
 import styles from '@/styles/login.module.css'
+import Link from 'next/link'
 
 export default function Home() {
-    return (
-        <div className={styles.background}>
-            Perfil
-        </div>
-    )
+  return (
+    <div className={styles.background}>
+      <h1>Seja Bem-vindo ao Next Register!</h1>
+      <p>Este é um aplicativo web para gerenciar registros.</p>
+      <Link href="/login">Entre agora</Link>
+    </div>
+  )
 }
