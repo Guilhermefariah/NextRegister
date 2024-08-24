16927460f9025d91a70e597c2b21e3df63dd66c9
Guilhermefariah <guilhermefaria321@gmail.com>
2024-08-24 20:43:13 -0300

    RegisterForm page with form submission and link update

--- src/pages/register.tsx
+++ src/pages/register.tsx
@@ -1,21 +1,60 @@
 import Link from "next/link"
+import { useState } from "react"
+
 import LoginCard from '@/components/loginCard'
-import styles from '@/styles/login.module.css'
 import Input from "@/components/ui/input/input"
 import Button from "@/components/ui/button/button"
 
+import styles from '@/styles/login.module.css'
+
 export default function RegisterForm() {
+    const [formData, setFormData] = useState({
+        name: '',
+        email: '',
+        password: ''
+    })
+
+    const handleFormChange = (event: React.ChangeEvent<HTMLInputElement>, name: string) => {
+        setFormData({
+            ...formData,
+            [name]: event.target.value
+        })
+    }
+
+    const handleFormSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        console.log(formData)
+    }
+
     return (
         <div className={styles.background}>
             <LoginCard title='Crie sua conta'>
-                <form className={styles.form}>
-                    <Input type='text' placeholder='Seu nome'/>
-                    <Input type="email" placeholder="Seu e-mail" />
-                    <Input type="password" placeholder="Sua senha" />
-                    <Button>Cadastrar</Button>
-                    <Link href='./login'>Já possui uma conta?</Link>
+                <form className={styles.form} onSubmit={handleFormSubmit}>
+                    <Input
+                        type='text'
+                        placeholder='Seu nome'
+                        required
+                        value={formData.name}
+                        onChange={(e) => handleFormChange(e, 'name')}
+                    />
+                    <Input
+                        type='email'
+                        placeholder='Seu e-mail'
+                        required
+                        value={formData.email}
+                        onChange={(e) => handleFormChange(e, 'email')}
+                    />
+                    <Input
+                        type='password'
+                        placeholder='Sua senha'
+                        required
+                        value={formData.password}
+                        onChange={(e) => handleFormChange(e, 'password')}
+                    />
+                    <Button type="submit">Cadastrar</Button>
+                    <Link href='/login'>Já possui uma conta?</Link>
                 </form>
             </LoginCard>
         </div>
     )
-} 
+}
